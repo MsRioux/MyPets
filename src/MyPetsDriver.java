28c4279c460028d021baf2f53fa29b38a2525269
MsRioux <jrioux@mvusd.net>
2020-01-23 13:39:14 -0800

    Added Bird class and tried to instantiate Pet

--- src/MyPetsDriver.java
+++ src/MyPetsDriver.java
@@ -4,8 +4,11 @@ public class MyPetsDriver
     {
         Dog rex = new Dog("Rex", "German Shepherd", 2, 160.2);
         Lizard mochi = new Lizard("Mochi", "Blue-Tongued Skink", 8, 2);
-
+        Bird donald = new Bird("Duck", "Donald", 85, 1.13, "white", 5000);
+        //Pet secret = new Pet("Secret", 7, 40.2); Pet is an abstract class and cannot be instantiated.
+        
         System.out.println(rex);
         System.out.println(mochi);
+        System.out.println(donald);
     }
 }
