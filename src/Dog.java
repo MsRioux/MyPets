b1afbab89b7ba44aa778eb88c0e7a5f2bb80e493
MsRioux <jrioux@mvusd.net>
2020-01-28 13:19:20 -0800

    Added fetch() to Dog to demonstrate Polymorphism issues with methods. Also discussed late and early binding.

--- src/Dog.java
+++ src/Dog.java
@@ -8,12 +8,21 @@ public class Dog extends Pet
         this.breed = breed;
     }//end full constructor with call to super
 
+    public String fetch()
+    {
+        if (Math.random() >= 0.5)
+            return "Fetch accomplished";
+        else
+            return "No fetching here";
+    }//end method that exists in Dog but not Pet to demo Polymorphism issue
+
+
     public String Speak()
     {
         if (this.age < 2)
             return "Yip";
         return "Woof";
-    }
+    }//end method Speak which overrides abstract method in Pet
 
     public String toString()
     {
--- src/MyPetsDriver.java
+++ src/MyPetsDriver.java
@@ -2,21 +2,35 @@ public class MyPetsDriver
 {
     public static void main(String [] args)
     {
-        Dog rex = new Dog("Rex", "German Shepherd", 2, 160.2);
-        Lizard mochi = new Lizard("Mochi", "Blue-Tongued Skink", 8, 2);
-        Bird donald = new Bird("Duck", "Donald", 85, 1.13, "white", 5000);
+        Pet rex = new Dog("Rex", "German Shepherd", 2, 160.2);
+        Pet mochi = new Lizard("Mochi", "Blue-Tongued Skink", 8, 2);
+        Pet donald = new Bird("Duck", "Donald", 85, 1.13, "white", 5000);
         //Pet secret = new Pet("Secret", 7, 40.2); Pet is an abstract class and cannot be instantiated.
 
+//        System.out.println(rex.fetch()); //only works when Rex is a Dog, NOT when rex is a Pet.
+        System.out.println(rex.Speak()); //dependent upon what type Rex, Dog vs Pet
+
+//        int[] arrayOfInts = new int[5]; // and example of how to instantiate an array
+//        Pet[]  menagerie = new Pet[5]; //Starting to create an array of pets
+//        menagerie[0] = rex;
+//        menagerie[1] = mochi;
+//        menagerie[2] = donald;
+//
+//        for( int i = 0; i < menagerie.length; i++)
+//        {
+//            System.out.println(menagerie[i]);
+//        }
+
         // Speaking to Pets
-        System.out.println("Rex says: " + rex.Speak());
-        System.out.println("Mochi says: " + mochi.Speak());
-        System.out.println("Donald says: " + donald.Speak());
+//        System.out.println("Rex says: " + rex.Speak());
+//        System.out.println("Mochi says: " + mochi.Speak());
+//        System.out.println("Donald says: " + donald.Speak());
 
         //Check unit conversion
-        System.out.println("Rex's weight in pounds is " + rex.AmericanFreedomUnits() + ".\n");
+//        System.out.println("Rex's weight in pounds is " + rex.AmericanFreedomUnits() + ".\n");
 
         //Testing Pets
-        System.out.println(rex);
+//        System.out.println(rex);
 //        System.out.println(mochi);
 //        System.out.println(donald);
     }
